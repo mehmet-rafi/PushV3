327cb973a56895fc64636885c8d131e72f2a6a2b
Mehmet Rafi Doğ <mrafido@gmail.com>
2013-07-30 14:55:16 +0300

    Project Start

---
+++ src/net/maksimum/utils/HelloGit.java
@@ -0,0 +1,15 @@
+package net.maksimum.utils;
+
+public class HelloGit {
+	
+	private String gitName = "GitHub";
+
+	public String getGitName() {
+		return gitName;
+	}
+
+	public void setGitName(String gitName) {
+		this.gitName = gitName;
+	}
+
+}
